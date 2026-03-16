9b528c3507adf832f57407df73f3700289f29629
Adam Hilty <hilty.89@osu.edu>
2026-03-16 13:38:19 -0400

    Added documentation

--- src/types/api.ts
+++ src/types/api.ts
@@ -1,13 +1,22 @@
+/**
+ * A course entry used for GPA calculations
+ */
 export interface Course {
     courseName: string;
     creditHours: number;
     grade: string;
 }
 
+/**
+ * The data structure sent to the API to request a GPA calculation.
+ */
 export interface GpaCalculationRequest {
     courses: Course[];
 }
 
+/**
+ * The data structure received from the API after a GPA calculation is completed.
+ */
 export interface GpaCalculationResponse {
     calculatedGpa: number;
     message: string;
