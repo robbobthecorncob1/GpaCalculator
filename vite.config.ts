c8bab543c58789e9b9b93a4673876f784273916e
Adam Hilty <hilty.89@osu.edu>
2026-03-18 23:57:29 -0400

    Added vitest to project

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+/// <reference types="vitest" />
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true, // Allows us to use describe, expect, and it without importing them every time
+    environment: 'jsdom', // Tells Vitest to run in a browser-like environment
+    setupFiles: './src/setupTests.ts', // We will create this file next!
+  },
 })
